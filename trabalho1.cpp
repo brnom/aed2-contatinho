0ef7468030331862cd07171e3cec3bd7bef677fd
unknown <brunomengaldo@gmail.com>
2019-09-30 07:42:30 -0700

    fuckin done

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -11,10 +11,9 @@ typedef unordered_map<string,int> Dicio;
 /* Requesitos:
  * - se existir contato, imprimir "Contatinho encontrado: telefone _tel"
  * - caso nao exista, imprimir "Contatinho nao encontrado"
-*/
+
 void PesquisaContato(Dicio &cont, string _nam){
     Dicio::iterator itr;
-    
     // procura e se achar, printa telefone
     for (itr = cont.begin(); itr != cont.end(); ++itr){
         if((itr->first).compare(_nam)==0){
@@ -22,19 +21,17 @@ void PesquisaContato(Dicio &cont, string _nam){
             return;
         }
     }
-
     // se nao achou, printa aq
     cout<<"Contatinho nao encontrado"<<endl;
-}
+}*/
 
 /* Requesitos:
  * - nao inserir pessoas com nomes iguais
  * - se for igual, imprimir "Contatinho ja inserido"
-*/
+
 void InsereContato(Dicio &cont, string _nam, int _tel){
 
     Dicio::iterator itr;
-    
     // procura e se achar, printa telefone
     for (itr = cont.begin(); itr != cont.end(); ++itr){
         if((itr->first).compare(_nam)==0){
@@ -42,38 +39,32 @@ void InsereContato(Dicio &cont, string _nam, int _tel){
             return;
         }
     }
-
     //se nao achou, insere
-    cont.insert(pair<string,int>(_nam, _tel));
-
-    
-}
+    cont.insert(pair<string,int>(_nam, _tel));   
+}*/
 
 /* Requesitos:
  * - se nao existir contato, imprimir "Operacao invalida: contatinho nao encontrado"
-*/
+
 void RemoveContato(Dicio &cont, string _nam){
     
     Dicio::iterator itr;
-    
     // procura e se achar, apaga
     for (itr = cont.begin(); itr != cont.end(); ++itr){
         if((itr->first).compare(_nam)==0){
             cont.erase(_nam);
             return;
         }
-    }
-    
+    }    
     cout << "Operacao invalida: contatinho nao encontrado" <<endl;
-}
+}*/
 
 /* Requesitos:
  * - se nao existir contato, imprimir "Operacao invalida: contatinho nao encontrado"
-*/
+
 void AlteraContato(Dicio &cont, string _nam, int _tel){
 
     Dicio::iterator itr;
-
     // procura e se achar, altera telefone
     for (itr = cont.begin(); itr != cont.end(); ++itr){
         if((itr->first).compare(_nam)==0){
@@ -81,57 +72,81 @@ void AlteraContato(Dicio &cont, string _nam, int _tel){
             return;
         }
     }
-
     cout << "Operacao invalida: contatinho nao encontrado" <<endl;
-}
+}*/
 
-vector<string> corta(const string& s, char delimita){
-    vector<string> tokens;
-    string tok;
-    istringstream algo(s);
-    while(getline(algo, tok, delimita)){
-        tokens.push_back(tok);
-    }
-    return tokens;
-}
+void operaciones(Dicio &cont, char cmd, string _nam, int _tel){
+    
+        Dicio::iterator itr;
+        
+        //Insere 
+        if(cmd=='I'){
+            itr = cont.find(_nam);
+            if (itr != cont.end())
+                cout << "Contatinho ja inserido" << endl;
+            else
+                cont.insert(pair<string,int>(_nam, _tel));
+            return;
+        }
 
+        //Pesquisa
+        if(cmd=='P'){
+            itr = cont.find(_nam);
+            if (itr != cont.end())
+                cout <<"Contatinho encontrado: telefone "<< itr->second <<endl;
+            else
+                cout<<"Contatinho nao encontrado"<<endl;
+            return;
+        }
+            
+
+        //Remove
+        if(cmd=='R'){
+            // procura e se achar, apaga
+            itr = cont.find(_nam);
+            if (itr != cont.end())
+                cont.erase (itr);
+            else
+                cout << "Operacao invalida: contatinho nao encontrado" <<endl;
+            return;
+
+        }
+
+
+        //Altera
+        if(cmd=='A'){
+            // procura e se achar, altera telefone            
+            itr = cont.find(_nam);
+            if (itr != cont.end())
+                itr->second=_tel;
+            else
+                cout << "Operacao invalida: contatinho nao encontrado" <<endl;
+            return;
+        }
+
+}
 
 int main(int argc, char *argv[]){
     
     Dicio contatinhos;
+    char cmd;
+    string nome;
+    int tel;
+
 
     while (1){
-        //cout << "entrada:: ";
-        int number=0;
-        string all;
-        getline(cin, all);
-    
 
-        //Tratamento de dados
-        vector<string> args;
-        args=corta(all, ' ');
-        if(args.size()==3)
-            number=stoi(args[2]);
+        cin>>cmd;
 
-        //Insere 
-        if(args[0].compare("I")==0)
-            InsereContato(contatinhos, args[1], number);                
-
-        //Pesquisa
-        else if(args[0].compare("P")==0)
-            PesquisaContato(contatinhos, args[1]);                
-
-        //Remove
-        else if(args[0].compare("R")==0)
-            RemoveContato(contatinhos, args[1]);
-
-        //Altera
-        else if(args[0].compare("A")==0)
-            AlteraContato(contatinhos, args[1], number);
-
-        //Para
-        else if(args[0].compare("0")==0)
+        if(cmd=='0')
             return 0;
+
+        cin>>nome;
+        if(cmd=='I'||cmd=='A')
+            cin>>tel;
+
+        operaciones(contatinhos, cmd, nome, tel);
+
+
     }
-    return 0;
 }
